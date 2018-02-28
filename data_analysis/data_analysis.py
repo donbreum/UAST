eb0ddc2314b716d0b1c61c46855087c28f2c0d19
Javier <jlrandulfe@gmail.com>
2018-02-28 15:41:21 +0100

    Add a polynomial fitter for fitting the values

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -4,22 +4,32 @@ import bokeh.plotting as bkplot
 import numpy as np
 
 
-def plot_data(input_data, title="Test title", fname="result"):
+def plot_data(input_data, show_result=True, title="Title", fname="result"):
     bkplot.output_file("./results/{}.html".format(fname))
-    p = bkplot.figure(title=title, x_axis_label='duty cycle',
-               y_axis_label='thust')
+    plt = bkplot.figure(title=title, x_axis_label='duty cycle',
+                        y_axis_label='thust')
+    # Parse the values to plot from the input data.
     x = input_data[0, :, 0]
     y0 = input_data[0, :, 1]
     y1 = input_data[1, :, 1]
     y2 = input_data[2, :, 1]
-
-    p.line(x, y0, legend="test 1", line_color="green")
-    p.circle(x, y0, legend="test 1", fill_color="white", line_color="green", size=8)
-    p.line(x, y1, legend="test 2", line_color="blue")
-    p.circle(x, y2, legend="test 3", fill_color="red", line_color="red", size=6)
-    p.line(x, y2, legend="test 3", line_color="orange", line_dash="4 4")
-
-    bkplot.show(p)
+    y_mean = input_data[:, :, 1].mean(axis=0)
+    # Get the polynomial fitting for the data average
+    poly_coefs = np.polyfit(x, y_mean, 2)
+    poly_parser = np.poly1d(poly_coefs)
+    y_fitted = poly_parser(x)
+    # Draw the plots on a bakeh figure
+    plt.circle(x, y0, legend="test 1", fill_color="white", line_color="green",
+               size=6)
+    plt.circle(x, y1, legend="test 2", fill_color="orange", line_color="black",
+               size=6)
+    plt.circle(x, y2, legend="test 3", fill_color="red", line_color="red",
+               size=6)
+    plt.line(x, y_mean, legend="average", line_color="blue")
+    plt.line(x, y_fitted, legend="fitted", line_color="red", line_width=2)
+    plt.legend.location = "bottom_right"
+    if show_result:
+        bkplot.show(plt)
     return
 
 
