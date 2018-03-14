81e5ffa86949c6b761a4b169d07d40e2da99aedd
Per Breum <lillep3r@hotmail.com>
2018-03-14 18:26:16 +0100

    add plot for satellites and altitude

--- w11-UAS_navigation/exercise_nmea_data/nmea_read.py
+++ w11-UAS_navigation/exercise_nmea_data/nmea_read.py
@@ -28,33 +28,95 @@
 #****************************************************************************/
 '''
 2018-03-13 Kjeld First version, that reads in CSV data
+2018-03-14 Per Breum - add plot function
 '''
 
+from pylab import ion
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from datetime import datetime
+import time
+
 class nmea_class:
-  def __init__(self):
-    self.data = []
+    def __init__(self):
+        self.data = []
 
-  def import_file(self, file_name):
-    file_ok = True
-    try:
-      # read all lines from the file and strip \n
-      lines = [line.rstrip() for line in open(file_name)]
-    except:
-      file_ok = False
-    if file_ok == True:
-      pt_num = 0
-      for i in range(len(lines)): # for all lines
-        if len(lines[i]) > 0 and lines[i][0] != '#': # if not a comment or empty line
-          csv = lines[i].split (',') # split into comma separated list
-          self.data.append(csv)
+    def import_file(self, file_name):
+        file_ok = True
+        try:
+          # read all lines from the file and strip \n
+          lines = [line.rstrip() for line in open(file_name)]
+        except:
+          file_ok = False
+        if file_ok == True:
+          pt_num = 0
+          for i in range(len(lines)): # for all lines
+            if len(lines[i]) > 0 and lines[i][0] != '#': # if not a comment or empty line
+              csv = lines[i].split (',') # split into comma separated list
+              self.data.append(csv)
 
-  def print_data(self):
-    for i in range(len(self.data)):
-      print self.data[i]
+    def print_data(self):
+        for i in range(len(self.data)):
+          print self.data[i]
+
+    # use timestamp to calculate seconds for time axis
+    def get_time_array(self):
+        time = []
+        start_time = self.data[0][1]
+        seconds_start = 60 * 60 * int(start_time[0:2]) + 60 * int(start_time[2:4]) + int(start_time[4:6])
+
+        for i in range(len(self.data)-1):
+            hour = int(self.data[i][1][0:2]) * 60 * 60
+            minutes = int(self.data[i][1][2:4]) * 60
+            seconds = int(self.data[i][1][4:6])
+            time = np.append(time, (hour + minutes + seconds)-seconds_start)
+        return time
+
+    def get_column(self, col_num):
+        column = []
+        for i in range(len(self.data)-1):
+            column = np.append(column, self.data[i][col_num])
+        return column
+
+    def plot_data(self, x, y, title, xlabel, ylabel, filename_save):
+        plt.figure(1)
+        plt.title(title, fontsize=23)
+        plt.plot(x, y, linewidth=1.0)
+
+        y_max = np.asfarray(max(y), int) * 1.1
+        y_min = np.asfarray(min(y), int) * 0.9
+        print(y_max)
+        plt.ylim(y_min, y_max)
+        plt.xlim(0, x[-1])
+        plt.xlabel(xlabel, fontsize=20)
+        plt.ylabel(ylabel, fontsize=20)
+        plt.show()
+        plt.savefig(filename_save)
+        print('Press enter to quit')
+        input()
+
+    def plot_nmea_data(self):
+        ion()
+
+        # get seconds from timestamp difference from column 1
+        time = self.get_time_array()
+
+        # get altitude from column 9
+        altitude_msl = self.get_column(9)
+
+        # get number of satellites tracked from column
+        tracked_satellites = self.get_column(7)
+        print(tracked_satellites)
+
+        self.plot_data(time, altitude_msl, "Altitude above MSL measured over time", "Time in seconds", "Height in meters", "altitude_above_msl_measured_over_time")
 
 
 if __name__ == "__main__":
   print 'Importing file'
   nmea = nmea_class()
   nmea.import_file ('nmea_trimble_gnss_eduquad_flight.txt')
-  nmea.print_data()
+
+  #nmea.print_data()
+
+  nmea.plot_nmea_data()
