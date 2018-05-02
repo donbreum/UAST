3880ef9dcb214ccb4c2b0aff64b8422e69ccb450
Javier <jlrandulfe@gmail.com>
2018-05-02 10:41:39 +0200

    Add python code for getting values from user and sending through serial port

--- w18-remote-control/command-sender/main.py
+++ w18-remote-control/command-sender/main.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
 # Standard libraries
+import glob
 import serial
+import struct
+
+
+def send_integer(value, ser):
+    """
+    Send through serial port an integer formated as a 16-bit ushort.
+    """
+    message = struct.pack("I", value)
+    ser.write(message)
+    return
 
 
 def main():
     # Initialize and configure serial port
-    port = glob.glob("/dev/ttyUSB*")[0]
+    try:
+        port = glob.glob("/dev/ttyUSB*")[0]
+    except IndexError:
+        print("Any device detected. Aborting ...")
+        return
     ser =serial.Serial(port, 115200)
     ser.timeout = 0.5
-    
+    # Infinite loop for getting input from user and sending through port.
+    while(True):
+        usr_input = input("Enter a value between 0 and 65535: ")
+        try:
+            value = int(usr_input)
+        except ValueError:
+            print("Non valid input. Only integers")
+            continue
+        print("Sending {}".format(value))
+        send_integer(value, ser)
     return
 
 
